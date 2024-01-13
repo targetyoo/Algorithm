cd52e3af0a56bfb80231eab03520317c175fefc9
Weekoding <75559882+targetyoo@users.noreply.github.com>
2024-01-13 15:17:07 +0900

    [Bronze V] Title: 꼬마 정민, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.swift
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.swift
@@ -1,8 +1,10 @@
 let a: [Int] = readLine()!.split(separator:" ").map{Int($0)!}
-var result: Int = 0
 
-for i in a{
-   result += i   
-}
+print(a.reduce(0, +))
+// .reduce() : 고차함수. 배열의 요소들을 합치는 메소드로, 첫 번째 파라미터는 초기값, 두 번째 파라미터는 처리방법
 
-print(result)
+//var result: Int = 0
+//for i in a{
+//   result += i
+//}
+//print(result)
